9d2e2e1d477d441b6ef84ebebacd140cb1a01424
PavelNeof <pavelneof@gmail.com>
2023-01-06 12:15:52 +0300

    Create App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.scss';
+import React from "react";
+import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-App
-    </div>
-  );
+    return (
+        <div className="app">
+            <Router>
+                <Header></Header>
+                <Routes>
+                    <Route path={'/'} exact element={<Home />}/>
+                    <Route path={'/movie/:imdbId'} element={<MovieDetail/>}/>
+
+                    <Route path={'*'} element={<PageNotFound/>} />
+                </Routes>
+                <Footer/>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/common/apis/movieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey = 'fe0ec991';
