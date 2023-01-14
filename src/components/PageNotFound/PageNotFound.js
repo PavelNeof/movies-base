602eb44ec3be744b610d6788678a04c33e07ab7e
PavelNeof <pavelneof@gmail.com>
2023-01-14 15:44:34 +0300

    feat: page not found

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Navigate} from "react-router-dom";
+import "./PageNotFound.scss"
 
-const PageNotFound = () =>{
+const PageNotFound = () => {
+
+    let [count, setCount] = useState(5)
+
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCount(count = count - 1)
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId)
+        }
+
+    }, [])
+
+
+    if (count === 0) {
+        return <Navigate to={'/'}/>
+    }
 
     return (
-        <div>
-            PageNotFound
+        <div className={'container-box'}>
+            <h2 className={'text'}>Oops, page not found.
+                You will return to the main page in
+                <span>{` ${count} `}</span>
+                seconds
+            </h2>
         </div>
     )
 }
